9bd55276a8a0592ac418a3b02136d1dd9238e246
fvan-wij <fvan-wij@student.codam.nl>
2025-03-26 19:55:45 +0100

    Clean slate

--- src/routes/routes.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-
-export async function routes(fastify: FastifyInstance) {
-  fastify.get('/', async function(request: FastifyRequest, reply: FastifyReply) {
-    reply.send({ message: 'Hi from the Fastify server', method: request.method });
-  });
-};
