32de5dbac99883e63d7fc7e36b666720e5751339
smclacke <sarahmclacken@gmail.com>
2025-03-28 14:57:13 +0100

    starting with the prisma db, opening the endpoints, then ill get to querying from usermanagement

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import pluginCORS from '@fastify/cors';
 import pluginFormbody from '@fastify/formbody';
 import closeWithGrace from 'close-with-grace';
 
+
 const ADDRESS: string = process.env.LISTEN_ADDRESS ? process.env.LISTEN_ADDRESS : '0.0.0.0';
 const PORT: number = process.env.LISTEN_PORT ? parseInt(process.env.LISTEN_PORT, 10) : 3000;
 
@@ -30,6 +31,19 @@ fastify.register(pluginCORS), {
 
 fastify.register(routes);
 fastify.register(pluginFormbody);
+
+fastify.get("/users", async (_, reply) => {
+	try {
+		const response = await fetch("http://database:3000/users");
+		if (!response.ok) {
+			throw new Error("failed to fetch users");
+		}
+		const users = await response.json();
+		reply.send(users);
+	} catch (error) {
+		reply.status(500).send({error: "failed to fetch users"});
+	}
+});
 
 async function startServer() {
   // Delay is the number of milliseconds for the graceful close to finish
