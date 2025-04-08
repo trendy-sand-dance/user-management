7ef4d4ad955bc6212a857713f7100bd1d223e965
smclacke <sarahmclacken@gmail.com>
2025-04-08 16:43:44 +0200

    logout fully functioning

--- src/controllers/user/edit.controller.ts
+++ src/controllers/user/edit.controller.ts
@@ -4,18 +4,23 @@ const DATABASE_URL = 'http://database_container:3000';
 
 export const editUsername = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
 
-	const { username, password } = request.body as { username: string, password: string };
-	
-	const res = await fetch(`${DATABASE_URL}/editUsername/${username}`, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ username, password }),
-	});
-	if (!res.ok) {
-		const errorText = await res.text();
-		throw new Error(`Failed to edit user: ${res.status} ${errorText}`);
+	const { username } = request.params as { username: string };
+	try {
+
+		const res = await fetch(`${DATABASE_URL}/editUsername/${username}`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ username }),
+		});
+		if (!res.ok) {
+			throw res.status;
+		}
+		return res.status;
+	} catch(error) {
+		const errStatus = error as number;
+		return reply.code(errStatus).send({ error: 'Failed to edit username' }); 
 	}
-	return await res.json();
+
 };
 
 //export const editPassword = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
@@ -33,3 +38,6 @@ export const editUsername = async (request: FastifyRequest, reply: FastifyReply)
 //	}
 //	return await res.json();
 //};
+
+
+// edit email
--- src/controllers/user/logout.controller.ts
+++ src/controllers/user/logout.controller.ts
@@ -4,14 +4,12 @@ const DATABASE_URL = 'http://database_container:3000';
 
 export const logout = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
 
-	const { username } = request.params as { username: string };
+	try {
+		const { username } = request.params as { username: string };
+		const res = await fetch(`${DATABASE_URL}/logout/${username}`);
 	
-	const res = await fetch(`${DATABASE_URL}/logout/${username}`);
-	
-	if (!res.ok) {
-		const errorText = await res.text();
-		throw new Error(`Failed to log user out: ${res.status} ${errorText}`);
+		reply.code(200);
+	} catch (error) {
+		return reply.code(500).send({ error: 'Failed to logout' });
 	}
-
-	return await res.json();
 };
