348c51a9e5a3938d4c91b61be65dc60e2719d112
smclacke <sarahmclacken@gmail.com>
2025-04-08 14:19:16 +0200

    we can register, login and get user specific dashboard, hooray! next, clean up a little (triple check the post/gets and remove unncessary stuff from frontend login controller, then logout + edit ect

--- src/controllers/user/login.controller.ts
+++ src/controllers/user/login.controller.ts
@@ -5,7 +5,7 @@ const DATABASE_URL = 'http://database_container:3000';
 export const login = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
 
 	const { username, password } = request.body as { username: string, password: string };
-	console.log(`username: ${username}, password: ${password}`);
+	//console.log(`username: ${username}, password: ${password}`);
 
 	const res = await fetch(`${DATABASE_URL}/login`, {
 		method: 'POST',
@@ -16,6 +16,19 @@ export const login = async (request: FastifyRequest, reply: FastifyReply): Promi
 		const errorText = await res.text();
 		throw new Error(`Failed to log user in: ${res.status} ${errorText}`);
 	}
+	//console.log("Logging res.json:", res.body);
+	//const jsonBlob = await res.json();
+	//const resText = await res.text();
+	//console.log("text = ", resText);
+	//jsonBlob.user
+	//console.log("body - ", res.body);
+	//console.log("text - ", res.text);
+	return res.status;
 
-	return await res.json();
+	//return reply.code(res.status).send(jsonBlob);
+	//message: string, error: string, statusCode: number, email: string, avatar: string 
+	//return 
+
+	//console.log("blob user = ", jsonBlob.user);
+	//return reply.code(200).send(res);
 };
--- src/controllers/web/dash.controller.ts
+++ src/controllers/web/dash.controller.ts
@@ -4,9 +4,9 @@ const DATABASE_URL = 'http://database_container:3000';
 
 export const dash = async (request: FastifyRequest, reply: FastifyReply): Promise<any> => {
 
-	const { username } = request.body as { username: string };
+	const { username } = request.params as { username: string };
 	
-	const res = await fetch(`${DATABASE_URL}/dash/${username}`);
+	const res = await fetch(`${DATABASE_URL}/dashboard/${username}`);
 	if (!res.ok) {
 		const errorText = await res.text();
 		throw new Error(`Failed to load user dashboard: ${res.status} ${errorText}`);
@@ -14,5 +14,3 @@ export const dash = async (request: FastifyRequest, reply: FastifyReply): Promis
 	return await res.json();
 };
 
-
-/** sending user member back from database, how/where access the rest of the info? */
